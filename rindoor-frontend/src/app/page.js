d248a1383568ed5626863ae1ee1a842399df6e19
leandro mortarini <leandromortarini2@gmail.com>
2024-05-07 17:48:34 -0300

    APP

---
+++ rindoor-frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <h1 className="text-6xl "> Rin|Door</h1>
+    </div>
+  );
+}
