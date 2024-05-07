471566d90de4c4910dc74976f402522551da18c4
leandro mortarini <leandromortarini2@gmail.com>
2024-05-07 18:37:18 -0300

    NavBar y footer

--- rindoor-frontend/src/app/layout.js
+++ rindoor-frontend/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NavBar } from "@/Components/NavBar/NavBar";
+import { Footer } from "@/Components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
