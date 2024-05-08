fa3176b0d2d9f4f8c28963bc0abf17eb59fc9fcf
Agustina <agustinaayelensosa@gmail.com>
2024-05-08 18:29:39 -0300

    carrusel implementado

--- rindoor-frontend/src/app/page.js
+++ rindoor-frontend/src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="w-full min-h-screen flex flex-col justify-start items-center bg-gradient-to-r from-yellow-300 via-yellow-100 to-yellow-300 h-screen">
       {/* <div className="w-full bg-yellow-600 h-[300px]"></div> */}
 
-      {/* <Carousel /> */}
+      <Carousel />
       <Categories />
     </div>
   );
