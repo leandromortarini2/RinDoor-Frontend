d8bd59dbd8885c73362595e39e9365a12cfbc209
leandro mortarini <leandromortarini2@gmail.com>
2024-05-08 15:36:39 -0300

    Img

--- rindoor-frontend/src/app/page.js
+++ rindoor-frontend/src/app/page.js
@@ -1,10 +1,14 @@
+"use client";
 import Image from "next/image";
 import { Categories } from "@/Components/Categories/Categories";
+import Carousel from "@/Components/Carousel/Carousel";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex flex-col justify-start items-center bg-white">
-      <div className="w-full bg-yellow-600 h-[300px]"></div>
+    <div className="w-full min-h-screen flex flex-col justify-start items-center bg-gradient-to-r from-gray-700 via-gray-300 to-gray-700 h-screen">
+      {/* <div className="w-full bg-yellow-600 h-[300px]"></div> */}
+
+      <Carousel />
       <Categories />
     </div>
   );
