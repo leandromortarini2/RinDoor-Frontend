8cad661759115d31f69f78e00f7acc3ead1adc31
Agustina <agustinaayelensosa@gmail.com>
2024-05-09 15:51:23 -0300

    carrusel y about

--- rindoor-frontend/tailwind.config.js
+++ rindoor-frontend/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        amarilloBG: '#eab308',
+        azul: '#111827'
+      }
     },
   },
   plugins: [],
